d680cb64284e0fd4b82a41e5c14264c6bd9f7121
Sgess <soraiyakg@yahoo.com>
2020-01-06 17:52:48 -0800

    Update plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,93 +23,23 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Initial Params
-var chosenXAxis = "income";
-
-// function used for updating x-scale var upon click on axis label
-function xScale(data, chosenXAxis) {
-  // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
-      d3.max(data, d => d[chosenXAxis]) * 1.2
-    ])
-    .range([0, width]);
-
-  return xLinearScale;
-
-}
-
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-  var bottomAxis = d3.axisBottom(newXScale);
-
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
-
-  return xAxis;
-}
-
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, chosenXaxis) {
-
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]));
-
-  return circlesGroup;
-}
-
-// function used for updating circles group with new tooltip
-function updateToolTip(chosenXAxis, circlesGroup) {
-
-    var label = "Household Income (Median)";
-
-//   if (chosenXAxis === "income") {
-//     var label = "Household Income (Median)";
-//   }
-//   else if (chosenXAxis === "poverty") {
-//       var label = "In Poverty (%)";
-//   }
-//   else {
-//     var label = "Age (Median)";
-//   }
-
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (`${d.state}`);
-    });
-
-  circlesGroup.call(toolTip);
-
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
-
-  return circlesGroup;
-}
-
 // Retrieve data from the CSV file and execute everything below
 d3.csv("assets/data/data.csv").then(function(data, err) {
   if (err) throw err;
 
   // parse data
   data.forEach(function(data) {
-    // data.poverty = +data.poverty;
-    // data.age = +data.age;
     data.obesity = +data.obesity;
     data.income = +data.income;
   });
 
   // xLinearScale function above csv import
-  var xLinearScale = xScale(data, chosenXAxis);
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d.income) * 0.8,
+    d3.max(data, d => d.income) * 1.2
+  ])
+  .range([0, width]);
+
 
   // Create y scale function
   var yLinearScale = d3.scaleLinear()
@@ -121,7 +51,7 @@ d3.csv("assets/data/data.csv").then(function(data, err) {
   var leftAxis = d3.axisLeft(yLinearScale);
 
   // append x axis
-  var xAxis = chartGroup.append("g")
+  chartGroup.append("g")
     .classed("x-axis", true)
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
@@ -135,29 +65,29 @@ d3.csv("assets/data/data.csv").then(function(data, err) {
     .data(data)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cx", d => xLinearScale(d.income))
     .attr("cy", d => yLinearScale(d.obesity))
     .attr("r", 15)
     .attr("fill", "lightblue")
     .attr("opacity", ".75");
 
-  // Create group for  2 x- axis labels
-  var labelsGroup = chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+  circlesGroup.append("g")
+    .selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(d => d.state)
+    .attr('font-size',8)//font size
+    .attr('dx', 0)//positions text towards the left of the center of the circle
+    .attr('dy',4);
 
-  var incomeLabel = labelsGroup.append("text")
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`)
     .attr("x", 0)
     .attr("y", 20)
-    .attr("value", "income") // value to grab for event listener
+    .attr("value", "income") 
     .classed("active", true)
     .text("Income (Median)");
-
-//   var albumsLabel = labelsGroup.append("text")
-//     .attr("x", 0)
-//     .attr("y", 40)
-//     .attr("value", "num_albums") // value to grab for event listener
-//     .classed("inactive", true)
-//     .text("# of Albums Released");
 
   // append y axis
   chartGroup.append("text")
@@ -168,53 +98,6 @@ d3.csv("assets/data/data.csv").then(function(data, err) {
     .classed("axis-text", true)
     .text("Obesity (%)");
 
-  // updateToolTip function above csv import
-  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-  // x axis labels event listener
-  labelsGroup.selectAll("text")
-    .on("click", function() {
-      // get value of selection
-      var value = d3.select(this).attr("value");
-      if (value !== chosenXAxis) {
-
-        // replaces chosenXAxis with value
-        chosenXAxis = value;
-
-        // console.log(chosenXAxis)
-
-        // functions here found above csv import
-        // updates x scale for new data
-        xLinearScale = xScale(data, chosenXAxis);
-
-        // updates x axis with transition
-        xAxis = renderAxes(xLinearScale, xAxis);
-
-        // updates circles with new x values
-        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-
-        // updates tooltips with new info
-        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-        // changes classes to change bold text
-        // if (chosenXAxis === "num_albums") {
-        //   albumsLabel
-        //     .classed("active", true)
-        //     .classed("inactive", false);
-        //   hairLengthLabel
-        //     .classed("active", false)
-        //     .classed("inactive", true);
-        // }
-        // else {
-        //   albumsLabel
-        //     .classed("active", false)
-        //     .classed("inactive", true);
-        //   hairLengthLabel
-        //     .classed("active", true)
-        //     .classed("inactive", false);
-        // }
-      }
-    });
 }).catch(function(error) {
   console.log(error);
 });
